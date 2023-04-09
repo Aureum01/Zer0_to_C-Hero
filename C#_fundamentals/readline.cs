5effe8dab3db8ae0c7c063fe06b2b8b4a221bd94
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2023-04-09 14:14:49 -0400

    Create readline.cs
    use 'dotnet run' in the bash shell prompt

---
+++ C#_fundamentals/readline.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
